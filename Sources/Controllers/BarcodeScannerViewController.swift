02fc7041a47427991ebfd664684a9b7205260a12
cdf1982 <cdf1982@users.noreply.github.com>
2021-07-13 15:46:39 +0200

    footerHeight as variable

--- Sources/Controllers/BarcodeScannerViewController.swift
+++ Sources/Controllers/BarcodeScannerViewController.swift
@@ -32,7 +32,7 @@ public protocol BarcodeScannerDismissalDelegate: class {
  - Not found error message
  */
 open class BarcodeScannerViewController: UIViewController {
-  private static let footerHeight: CGFloat = 75
+  var footerHeight: CGFloat = 75
   public var hideFooterView = false
 
   // MARK: - Public properties
